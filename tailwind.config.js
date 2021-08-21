c2dd1efc5a58856ba25eca8445b1bc3a9470e046
AJ <aj1305>
2021-08-21 20:47:44 +0100

    backup before dom manipulation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                gold: "#D2AC4A",
+                "gold": "#D2AC4A",
+                "gold-light": "#FFD700",
             },
         },
     },
