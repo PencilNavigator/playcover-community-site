88d641aa50e77c226f00a4339aa757499480144b
AJ <aj1305>
2021-08-23 18:53:13 +0100

    added language menu in mobile view

--- public/language-button.js
+++ public/language-button.js
@@ -6,3 +6,11 @@ const menu = document.querySelector(".language-menu");
 btn.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
 });
+
+const mobileLangBtn = document.querySelector("button.mobile-language-button");
+const mobileLangMenu = document.querySelector(".mobile-language-menu");
+
+// add event listeners
+mobileLangBtn.addEventListener("click", () => {
+	mobileLangMenu.classList.toggle("hidden");
+});
