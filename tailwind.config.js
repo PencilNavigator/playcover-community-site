b5cd656c07a85bc3cd3f746644c688e32777f6c6
AJ <aj1305>
2021-08-23 01:59:15 +0100

    footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    mode: "jit",
-    purge: ["./public/**/*.html"],
-    darkMode: "media", // false or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                gold: "#D2AC4A",
-                "gold-light": "#FFD700",
-            },
-        },
-        fontFamily: {
-            body: '"Open Sans", Arial, sans-serif',
-            logo: "Roboto, Arial, sans-serif",
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	mode: "jit",
+	purge: ["./public/**/*.html"],
+	darkMode: "media", // false or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				gold: "#D2AC4A",
+				"gold-light": "#FFD700",
+			},
+		},
+		fontFamily: {
+			body: '"Roboto", Arial, sans-serif',
+			logo: "Roboto, Arial, sans-serif",
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
