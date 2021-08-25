ed7ab0ea921dbf1bd4ab1f9a35a7ed33c932c896
AJ <aj1305>
2021-08-25 09:38:24 +0100

    added animation in lang icon

--- public/js/language-button.js
+++ public/js/language-button.js
@@ -1,12 +1,21 @@
 // grab everything we need
 const btn = document.querySelector("button.language-button");
 const menu = document.querySelector(".language-menu");
+const langIcon = document.querySelector(".language-menu-icon");
 
 // add event listeners
 btn.addEventListener("click", () => {
-	menu.classList.toggle("-translate-y-2/4");
-	menu.classList.toggle("scale-0");
-	menu.classList.toggle("opacity-0");
+	if (menu.classList.contains("-translate-y-2/4")) {
+		menu.classList.remove("-translate-y-2/4");
+		menu.classList.remove("scale-0");
+		menu.classList.remove("opacity-0");
+		langIcon.classList.add("rotate-180");
+	} else {
+		menu.classList.add("-translate-y-2/4");
+		menu.classList.add("scale-0");
+		menu.classList.add("opacity-0");
+		langIcon.classList.remove("rotate-180");
+	}
 });
 
 const mobileLangBtn = document.querySelector("button.mobile-language-button");
