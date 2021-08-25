82d7d750ecf12bc534979b0ef97f9a037dc4cc26
AJ <aj1305>
2021-08-25 14:31:17 +0100

    dark mode test

--- public/js/mobile-button.js
+++
@@ -1,8 +0,0 @@
-// // grab everything we need
-const mobileBtn = document.querySelector("button.mobile-button");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-// add event listeners
-mobileBtn.addEventListener("click", () => {
-	mobileMenu.classList.toggle("hidden");
-});
