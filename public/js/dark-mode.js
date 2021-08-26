2205a2e5bb414519bb30164e92425b45da0dd3d8
AJ <aj1305>
2021-08-26 15:28:48 +0100

    auto dark mode fix 2

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -17,7 +17,9 @@ themeBtn.addEventListener("click", () => {
 	let localTheme = localStorage.getItem("theme");
 	if (localTheme == null || localTheme == "light") {
 		localStorage.setItem("theme", "dark");
+		document.querySelector("html").classList.add("dark");
 	} else if (localTheme == "dark") {
 		localStorage.setItem("theme", "light");
+		document.querySelector("html").classList.remove("dark");
 	}
 });
