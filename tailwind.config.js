d074a3a77c9eb17536502c20f3e4f3c8d60665a0
AJ <aj1305>
2021-09-04 07:37:24 +0100

    added floating blobs behind screenshot

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,28 @@ module.exports = {
 	darkMode: "class", // false or 'media' or 'class'
 	theme: {
 		extend: {
+			animation: {
+				blob: "blob 6s infinite",
+			},
+
+			keyframes: {
+				blob: {
+					"0%": {
+						transform: "translate(0px, 0px) scale(1)",
+					},
+					"33%": {
+						transform: "translate(30px, -50px) scale(1.1)",
+					},
+					"66%": {
+						transform: "translate(-20px, 20px) scale(0.9)",
+					},
+
+					"100%": {
+						transform: "translate(0px, 0px) scale(1)",
+					},
+				},
+			},
+
 			colors: {
 				gold: "#D2AC4A",
 				"gold-light": "#FFD700",
