b3857ccfd599f4740a6113f019306656c477cdf4
AJ <aj1305>
2021-09-04 02:10:00 +0100

    modified coming soon location

--- public/js/animator.js
+++ public/js/animator.js
@@ -2,9 +2,9 @@ if (localStorage.getItem("visited") == null) {
 	console.log("WORKING");
 	localStorage.setItem("visited", true);
 
-	themeBtn.classList.add("animate-bounce");
+	themeBtn.classList.add("animate-pulse");
 	setTimeout(() => {
-		themeBtn.classList.remove("animate-bounce");
+		themeBtn.classList.remove("animate-pulse");
 	}, 6000);
 
 	themeBtnMob.classList.add("animate-pulse");
