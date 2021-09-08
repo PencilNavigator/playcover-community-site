3a09cb5bb04fe79edc227c055ecc3c7f104faf3d
AJ <aj1305>
2021-09-08 19:52:54 +0100

    combined js files into one root folder

--- public/faq/js/local-storage-track.js
+++
@@ -1,9 +0,0 @@
-if (localStorage.getItem("theme") == "dark") {
-	if (!document.querySelector("html").classList.contains("dark")) {
-		document.querySelector("html").classList.add("dark");
-	}
-} else if (localStorage.getItem("theme") == "light") {
-	if (document.querySelector("html").classList.contains("dark")) {
-		document.querySelector("html").classList.remove("dark");
-	}
-}
