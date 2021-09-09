512673b41b70c289692a7ac6811df9e2fb407c06
AJ <aj1305>
2021-09-09 01:00:10 +0100

    fixed auto dark mode on mobile

--- public/js/local-storage-track.js
+++ public/js/local-storage-track.js
@@ -1,8 +1,10 @@
-// auto detect dark mode
-if (localStorage.getItem("theme") == null) {
-	if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-		localStorage.setItem("theme", "dark");
-		document.querySelector("html").classList.add("dark");
+// auto detect dark mode on screens greater than 1280px
+if (window.screen.width > 1280) {
+	if (localStorage.getItem("theme") == null) {
+		if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			localStorage.setItem("theme", "dark");
+			document.querySelector("html").classList.add("dark");
+		}
 	}
 }
 
