42d4a5dfb374dde48a10a638466912c22767faf5
AJ <aj1305>
2021-09-14 16:23:18 +0100

    modified language button bg

--- public/js/scroll-animations.js
+++ public/js/scroll-animations.js
@@ -7,12 +7,23 @@ window.addEventListener("scroll", (e) => {
         navBar.classList.add("dark:bg-nav-footer");
         navBar.classList.add("bg-white");
         navBar.classList.add("drop-shadow-md");
+
+        menu.classList.add("bg-white");
+        menu.classList.add("dark:bg-nav-footer");
+        menu.classList.remove("bg-gray-100");
+        menu.classList.remove("bg-body");
+        // bg-white dark:bg-nav-footer
     } else {
         navBar.classList.add("bg-gray-100");
         navBar.classList.add("dark:bg-body");
         navBar.classList.remove("dark:bg-nav-footer");
         navBar.classList.remove("bg-white");
         navBar.classList.remove("drop-shadow-md");
+
+        menu.classList.remove("bg-white");
+        menu.classList.remove("dark:bg-nav-footer");
+        menu.classList.add("bg-gray-100");
+        menu.classList.add("bg-body");
     }
 
 })
