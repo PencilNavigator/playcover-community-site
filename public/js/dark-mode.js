b80570825e1c447e1743cb817f7e9f9eca3a9c88
AJ <aj1305>
2021-09-14 19:06:35 +0100

    added new dark mode toggle button in desktop design

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,5 +1,23 @@
 const htmlTag = document.querySelector("html");
 const themeBtn = document.querySelector(".switch-theme");
+const themeToggleButton = document.querySelectorAll(".theme-label");
+const toggleBall = document.querySelectorAll(".ball");
+
+
+themeToggleButton.forEach((toggleButton) => {
+	toggleButton.addEventListener("click", () => {
+		let localTheme = localStorage.getItem("theme");
+		if (localTheme == null || localTheme == "light") {
+			localStorage.setItem("theme", "dark");
+			document.querySelector("html").classList.add("dark");
+		} else if (localTheme == "dark") {
+			localStorage.setItem("theme", "light");
+			document.querySelector("html").classList.remove("dark");
+		}
+	})
+
+})
+
 
 themeBtn.addEventListener("click", () => {
 	// stop bounce animation
