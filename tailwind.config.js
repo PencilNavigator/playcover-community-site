fc95cba2e081da75164e771b39ba267edcf9237a
AJ <aj1305>
2021-09-15 08:06:08 +0100

    adjusted fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,8 +36,8 @@ module.exports = {
 			},
 		},
 		fontFamily: {
-			sourceSansPro: "'Source Sans Pro', Calibri, sans-serif;",
-			openSans: "'Open Sans', Calibri, sans-serif",
+			sourceSansPro: "'Source Sans Pro', Arial, sans-serif;",
+			openSans: "'Open Sans', Arial, sans-serif",
 		},
 	},
 	variants: {
