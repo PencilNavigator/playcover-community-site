5fb542d2fa7c1bc447128cd35da490a23d172a8c
AJ <aj1305>
2021-09-16 22:03:44 +0100

    added some js files to test google bot issue

---
+++ public/js/auto-closer.js
@@ -0,0 +1,24 @@
+const autoCloser = document.querySelector(".auto-closer");
+
+const toggleAutoCloser = function (autoCloser) {
+	autoCloser.classList.toggle("hidden");
+};
+
+// calc page height
+const body = document.querySelector("body");
+const html = document.documentElement;
+const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+autoCloser.style.height = `${pageHeight}px`;
+
+autoCloser.addEventListener("click", () => {
+	if (isLangMenuOpen) {
+		hideLangMenu(menu);
+		isLangMenuOpen = false;
+	} else if (isMobileMenuOpen) {
+		toggleMobileMenu();
+		isMobileMenuOpen = false;
+	}
+
+	toggleAutoCloser(autoCloser);
+});
---
+++ public/js/contact-popup.js
@@ -0,0 +1,35 @@
+const contactPopup = document.querySelector(".contact-popup");
+const contactButtons = document.querySelectorAll(".contact-button");
+const contactCloseButton = document.querySelector(".contact-close-button");
+const contactNavButton = document.querySelector(".contact-nav-button");
+const mobileContactButton = document.querySelector(".mobile-contact-button");
+const contactWrapper = document.querySelector(".contact-wrapper");
+const contactModal = document.querySelector(".contact-modal");
+
+function toggleContactPopup() {
+	contactPopup.classList.toggle("hidden");
+	document.querySelector("body").classList.toggle("overflow-hidden");
+}
+
+function toggleContactPopupMob() {
+	// from the mobile-buttons js file
+	mobileMenu.classList.toggle("hidden");
+	toggleContactPopup();
+}
+
+contactCloseButton.addEventListener("click", toggleContactPopup);
+
+contactNavButton.addEventListener("click", toggleContactPopup);
+
+mobileContactButton.addEventListener("click", toggleContactPopupMob);
+
+contactButtons.forEach((contactButton) => {
+	contactButton.addEventListener("click", toggleContactPopup);
+});
+contactModal.addEventListener("click", (e) => {
+	e.stopPropagation();
+});
+
+contactWrapper.addEventListener("click", () => {
+	toggleContactPopup();
+});
