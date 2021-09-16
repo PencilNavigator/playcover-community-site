1d8b6c6042f0d7ce1c865caa2e75b85f7b6d63e2
AJ <aj1305>
2021-09-17 00:59:32 +0100

    shifted everything to root instead of public and updated robots.txt file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,49 @@
 module.exports = {
-	mode: "jit",
-	purge: ["./public/**/*.html"],
-	darkMode: "class", // false or 'media' or 'class'
-	theme: {
-		screens: {
-			sm: "640px",
-			// => @media (min-width: 640px) { ... }
+  mode: 'jit',
+  purge: ['./**/*.html'],
+  darkMode: 'class', // false or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
 
-			md: "768px",
-			// => @media (min-width: 768px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
 
-			lg: "1024px",
-			// => @media (min-width: 1024px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
 
-			xl: "1280px",
-			// => @media (min-width: 1280px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
 
-			"2xl": "1536px",
-			// => @media (min-width: 1536px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
 
-			"3xl": "1900px",
-			// => @media (min-width: 1536px) { ... }
-		},
-		extend: {
-			colors: {
-				gold: "#D2AC4A",
-				"gold-light": "#FFD700",
-				"pastel-pink": "#9c2f64",
-				"menu-pink": "#E23B8B",
-				"logo-pink": "#FF0066",
-				"logo-purple": "#b634e9",
-				"logo-purple-dark": "#952abf",
-				"logo-purple-darker": "#7f24a3",
-				"text-purple": "#9861ff",
-				"body": "#171717",
-				"nav-footer": "#141414",
-			},
-		},
-		fontFamily: {
-			sourceSansPro: "'Source Sans Pro', Arial, sans-serif;",
-			openSans: "'Open Sans', Arial, sans-serif",
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+      '3xl': '1900px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        gold: '#D2AC4A',
+        'gold-light': '#FFD700',
+        'pastel-pink': '#9c2f64',
+        'menu-pink': '#E23B8B',
+        'logo-pink': '#FF0066',
+        'logo-purple': '#b634e9',
+        'logo-purple-dark': '#952abf',
+        'logo-purple-darker': '#7f24a3',
+        'text-purple': '#9861ff',
+        body: '#171717',
+        'nav-footer': '#141414',
+      },
+    },
+    fontFamily: {
+      sourceSansPro: "'Source Sans Pro', Arial, sans-serif;",
+      openSans: "'Open Sans', Arial, sans-serif",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
