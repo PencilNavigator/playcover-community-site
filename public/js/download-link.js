27d6568a0287847bd5387deb5ee8f15c9b97a6da
AJ <aj1305>
2021-09-16 21:59:41 +0100

    removed js file to test google bots

--- public/js/download-link.js
+++
@@ -1,8 +0,0 @@
-const downloadButtons = document.querySelectorAll(".download-buttons");
-
-// download link to latest release
-const downloadLink = "https://github.com/iVoider/PlayCover/releases/download/0.7.3/PlayCover.0.7.3.zip";
-
-downloadButtons.forEach((downloadButton) => {
-    downloadButton.setAttribute("href", downloadLink);
-})
