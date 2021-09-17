8fa785672d6f2a152b21dfdd233488e753c06855
AJ <aj1305>
2021-09-17 21:02:30 +0100

    added search in faq pages

---
+++ js/faq-search.js
@@ -0,0 +1,36 @@
+const searchBar = document.querySelector ('.search-bar');
+const searchField = document.querySelector ('.search-bar input');
+const questionsList = document.querySelectorAll ('.question .activator');
+
+const filterQuestions = searchTerm => {
+  Array.from (questionsList)
+    .filter (question => {
+      return !question.textContent.toLowerCase ().includes (searchTerm);
+    })
+    .forEach (question => {
+      question.parentElement.classList.add ('hidden');
+    });
+
+  Array.from (questionsList)
+    .filter (question => {
+      return question.textContent.toLowerCase ().includes (searchTerm);
+    })
+    .forEach (question => {
+      question.parentElement.classList.remove ('hidden');
+    });
+};
+
+const resetFilter = () => {
+  questionsList.forEach (question => {
+    question.parentElement.classList.remove ('hidden');
+  });
+};
+
+searchField.addEventListener ('keyup', () => {
+  const searchTerm = searchField.value.trim ().toLowerCase ();
+  if (searchTerm) {
+    filterQuestions (searchTerm);
+  } else {
+    resetFilter ();
+  }
+});
