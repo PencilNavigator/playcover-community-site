ca95d332d56b69e4485d658e04a2740480c4444e
AJ <aj1305>
2021-09-18 00:46:13 +0100

    updated release link

--- js/download-link.js
+++ js/download-link.js
@@ -2,7 +2,7 @@ const downloadButtons = document.querySelectorAll ('.download-buttons');
 
 // download link to latest release
 const downloadLink =
-  'https://github.com/iVoider/PlayCover/releases/download/0.7.3/PlayCover.0.7.3.zip';
+  'https://github.com/iVoider/PlayCover/releases/download/0.7.5/PlayCover.0.7.5.zip';
 
 downloadButtons.forEach (downloadButton => {
   downloadButton.setAttribute ('href', downloadLink);
