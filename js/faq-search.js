2cbf409cd4dee8f5b03259dd7d290142caf2ff03
JoseMoreville <18040006@utculiacan.edu.mx>
2021-09-23 18:05:50 -0600

    feat(js): Prevent default on forms, polyfill

--- js/faq-search.js
+++ js/faq-search.js
@@ -2,36 +2,52 @@ const searchField = document.querySelector('.search-bar input');
 const questionsList = document.querySelectorAll('.question .activator');
 
 const filterQuestions = (searchTerm) => {
-	Array.from(questionsList)
-		.filter((question) => {
-			return !question.textContent.toLowerCase().includes(searchTerm);
-		})
-		.forEach((question) => {
-			question.parentElement.classList.add('hidden');
-		});
+    Array.from(questionsList)
+        .filter((question) => {
+            return !question.textContent.toLowerCase().includes(searchTerm);
+        })
+        .forEach((question) => {
+            question.parentElement.classList.add('hidden');
+        });
 
-	Array.from(questionsList)
-		.filter((question) => {
-			const checkQuestion = question.textContent.toLowerCase().includes(searchTerm);
-			const checkAnswer = question.nextElementSibling.textContent.toLowerCase().includes(searchTerm);
-			return checkQuestion || checkAnswer;
-		})
-		.forEach((question) => {
-			question.parentElement.classList.remove('hidden');
-		});
+    Array.from(questionsList)
+        .filter((question) => {
+            const checkQuestion = question.textContent.toLowerCase().includes(searchTerm);
+            const checkAnswer = question.nextElementSibling.textContent.toLowerCase().includes(searchTerm);
+            return checkQuestion || checkAnswer;
+        })
+        .forEach((question) => {
+            question.parentElement.classList.remove('hidden');
+        });
 };
 
 const resetFilter = () => {
-	questionsList.forEach((question) => {
-		question.parentElement.classList.remove('hidden');
-	});
+    questionsList.forEach((question) => {
+        question.parentElement.classList.remove('hidden');
+    });
 };
 
 searchField.addEventListener('keyup', () => {
-	const searchTerm = searchField.value.trim().toLowerCase();
-	if (searchTerm) {
-		filterQuestions(searchTerm);
-	} else {
-		resetFilter();
-	}
+    const searchTerm = searchField.value.trim().toLowerCase();
+    if (searchTerm) {
+        filterQuestions(searchTerm);
+    } else {
+        resetFilter();
+    }
 });
+
+
+document.querySelectorAll('form').forEach(e => {
+    e.addEventListener('submit', form => {
+        form.preventDefault()
+        /*
+        const searchTerm = searchField.value.trim().toLowerCase();
+        questionsList.forEach(item => {
+            if (!(item.textContent.toLowerCase().includes(searchTerm) || item.nextElementSibling.textContent.toLowerCase().includes(searchTerm))) {
+                console.log(!(item.textContent.toLowerCase().includes(searchTerm) || item.nextElementSibling.textContent.toLowerCase().includes(searchTerm)))
+                searchField.value = ''
+            }
+        })
+        */
+    })
+})
